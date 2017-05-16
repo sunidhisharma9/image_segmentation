a37abd1fd42461cf62a9e154e5add4a8bcfcd0a7
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-05-16 18:00:08 -0300

    Updated make_statistics_question_2 to make the IC interval for knn

--- make_statistics_question_2.py
+++ make_statistics_question_2.py
@@ -2,11 +2,17 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy as np
 
-rgb_accuracies = pickle.load(open('result_pickles/accuracies_RGB.pickle', 'rb'))
-shape_accuracies = pickle.load(open('result_pickles/accuracies_SHAPE.pickle', 'rb'))
+rgb_accuracies_bayes = pickle.load(open('result_pickles/accuracies_RGB.pickle', 'rb'))
+shape_accuracies_bayes = pickle.load(open('result_pickles/accuracies_SHAPE.pickle', 'rb'))
 
-rgb_np = np.array(rgb_accuracies)
-shape_np = np.array(shape_accuracies)
+rgb_accuracies_knn = pickle.load(open('result_pickles/accuracies_RGB_knn.pickle', 'rb'))
+shape_accuracies_knn = pickle.load(open('result_pickles/accuracies_SHAPE_knn.pickle', 'rb'))
+
+rgb_np_bayes = np.array(rgb_accuracies_bayes)
+shape_np_bayes = np.array(shape_accuracies_bayes)
+
+rgb_np_knn = np.array(rgb_accuracies_knn)
+shape_np_knn = np.array(shape_accuracies_knn)
 
 def create_IC_interval(np_array):
     t_29_0475 = 2.0452
@@ -19,12 +25,24 @@ def create_IC_interval(np_array):
 
 print 'BAYES-----------'
 
-ic_bayes_rgb = create_IC_interval(rgb_np)
+ic_bayes_rgb = create_IC_interval(rgb_np_bayes)
 print 'RGB'
 print 'Mean', ic_bayes_rgb[0]
 print 'IC 95 [', ic_bayes_rgb[1], ',' , ic_bayes_rgb[2], ']'
 
-ic_bayes_shape = create_IC_interval(shape_np)
+ic_bayes_shape = create_IC_interval(shape_np_bayes)
 print 'SHAPE'
 print 'Mean', ic_bayes_shape[0]
 print 'IC 95 [', ic_bayes_shape[1], ',' , ic_bayes_shape[2], ']'
+
+print 'KNN-----------'
+
+ic_knn_rgb = create_IC_interval(rgb_np_knn)
+print 'RGB'
+print 'Mean', ic_knn_rgb[0]
+print 'IC 95 [', ic_knn_rgb[1], ',' , ic_knn_rgb[2], ']'
+
+ic_knn_shape = create_IC_interval(shape_np_knn)
+print 'SHAPE'
+print 'Mean', ic_knn_shape[0]
+print 'IC 95 [', ic_knn_shape[1], ',' , ic_knn_shape[2], ']'
