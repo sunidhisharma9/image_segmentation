a051f6fa86d3fc49fdb9d5924877c2ce0336fbd6
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-05-15 23:01:45 -0300

    Created code to make 30 fold with knn

--- Knn.py
+++ Knn.py
@@ -107,38 +107,98 @@ class KnnClassifier:
 
         return num_right/num_total
 
+##print 'Testing RGB view'
+##dv = DataVectorizer(filename='RGB_view.pickle')
+##tam_x = np.shape(dv.X)[0]
+##all_data = []
+##
+##print 'Creating Data set'
+##for i in xrange(tam_x):
+##    all_data.append([dv.X[i], dv.Y[i]])
+##
+##random.shuffle(all_data)
+##
+##len_all_data = len(all_data)
+##size_train = int(0.7*float(len_all_data))
+##size_validation = int(0.15*float(len_all_data))
+##size_test = int(0.15*float(len_all_data))
+##
+##train = all_data[:size_train]
+##validation = all_data[size_train+1:size_train + size_validation]
+##X_train = np.array([x for (x,y) in all_data])
+##Y_train = np.array([y for (x,y) in all_data])
+##X_validation = np.array([x for (x,y) in all_data])
+##Y_validation = np.array([y for (x,y) in all_data])
+##
+##accuracies = []
+##for i in xrange(49):
+##    print 'Iteration', i, 'of 50'
+##    K = 2*i + 1
+##    knn = KnnClassifier(X_train, Y_train, K)
+##    new_accuracy = knn.evaluate(X_validation, Y_validation)
+##    print 'New Accuracy', new_accuracy
+##    accuracies.append([K, new_accuracy])
+##
+##out = open('knn_accuracies.pickle', 'wb')
+##pickle.dump(accuracies, out)
+##out.close()
+
+
+#Begin
+
+def make_30_fold_test(data_vectorizer):
+    X = data_vectorizer.X
+    Y = data_vectorizer.Y
+    classes = data_vectorizer.classes
+    num_classes = len(classes)
+    X_for_class = []
+    X_index = [(index, x) for (index, x) in enumerate(X)]
+    for i in xrange(num_classes):
+        separated_X = [[x,i] for (index, x) in X_index if Y[index]==i]
+        random.shuffle(separated_X)
+        X_for_class.append(separated_X)
+
+    folds = []
+    for k in xrange(30):
+        low_index = k*10
+        high_index = low_index + 10
+        new_fold = []
+        for i in xrange(num_classes):
+            new_fold += X_for_class[i][low_index:high_index]
+        random.shuffle(new_fold)
+        folds.append(new_fold)
+
+    print 'Folds created'
+    accuracies = []
+    for k in xrange(30):
+        test = folds[k]
+        train = []
+        for i in xrange(30):
+            if i != k:
+                train += folds[i]
+        X_train = np.array([t[0] for t in train])
+        Y_train = np.array([t[1] for t in train]).ravel()
+        X_test = np.array([t[0] for t in test])
+        Y_test = np.array([t[1] for t in test]).ravel()
+
+        knn = KnnClassifier(X_train, Y_train, 1)
+        new_accuracy = knn.evaluate(X_test, Y_test)
+        print 'Accuracy', k, 'of 30', new_accuracy
+        accuracies.append(new_accuracy)
+    return accuracies
+
 print 'Testing RGB view'
 dv = DataVectorizer(filename='RGB_view.pickle')
-tam_x = np.shape(dv.X)[0]
-all_data = []
+accuracies_RGB = make_30_fold_test(dv)
+print 'Testing Shape view'
+dv = DataVectorizer(filename='shape_view.pickle')
+accuracies_SHAPE = make_30_fold_test(dv)
 
-print 'Creating Data set'
-for i in xrange(tam_x):
-    all_data.append([dv.X[i], dv.Y[i]])
+out_rgb = open('accuracies_RGB_knn.pickle', 'wb')
+out_shape = open('accuracies_SHAPE_knn.pickle', 'wb')
 
-random.shuffle(all_data)
+pickle.dump(accuracies_RGB, out_rgb)
+pickle.dump(accuracies_SHAPE, out_shape)
 
-len_all_data = len(all_data)
-size_train = int(0.7*float(len_all_data))
-size_validation = int(0.15*float(len_all_data))
-size_test = int(0.15*float(len_all_data))
-
-train = all_data[:size_train]
-validation = all_data[size_train+1:size_train + size_validation]
-X_train = np.array([x for (x,y) in all_data])
-Y_train = np.array([y for (x,y) in all_data])
-X_validation = np.array([x for (x,y) in all_data])
-Y_validation = np.array([y for (x,y) in all_data])
-
-accuracies = []
-for i in xrange(49):
-    print 'Iteration', i, 'of 50'
-    K = 2*i + 1
-    knn = KnnClassifier(X_train, Y_train, K)
-    new_accuracy = knn.evaluate(X_validation, Y_validation)
-    print 'New Accuracy', new_accuracy
-    accuracies.append([K, new_accuracy])
-
-out = open('knn_accuracies.pickle', 'wb')
-pickle.dump(accuracies, out)
-out.close()
+out_rgb.close()
+out_shape.close()
