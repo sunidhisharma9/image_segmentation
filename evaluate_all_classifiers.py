b9d20daecfcca8a77f50f23c04d55bfadf20037e
Felipe Navarro Balbino Alves <fnbalves@gmail.com>
2017-05-27 16:58:45 -0300

    Created code to use the new ClassifierTester

---
+++ evaluate_all_classifiers.py
@@ -0,0 +1,26 @@
+# By Felipe {fnba}, Paulo {pan2} e Debora {dca2}@cin.ufpe.br
+from pandas import index
+
+import pandas as pd
+import numpy as np
+import csv
+import pickle
+import warnings
+import random
+
+from classifiers.DataVectorizer import *
+from classifiers.ClassifierTester import *
+
+print 'Testing full view'
+dv = DataVectorizer(filename='result_pickles/full_view.pickle')
+accuracies_full = []
+for i in xrange(30):
+    print 'iteration', i, 'of 30-------------'
+    new_accuracies = ClassifierTester.make_n_fold_test(dv, 10)
+    accuracies_full.append(new_accuracies)
+
+out_full = open('result_pickles/accuracies_all.pickle', 'wb')
+
+pickle.dump(accuracies_full, out_full)
+
+out_full.close()
